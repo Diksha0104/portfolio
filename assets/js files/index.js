356814049125e549c74f01ba2699bcc8850aa42c
Diksha kushwah <dikshakushwah2001@gmail.com>
2024-07-05 19:09:07 +0530

    1 complete

--- assets/js files/index.js
+++ assets/js files/index.js
@@ -37,4 +37,25 @@ window.onscroll=() =>{
     menuIcon.classList.remove('fa-xmark');
     navbar.classList.remove('active');
 } 
+//   scroll revel-------------------------------------------------
+ScrollReveal({
+    distance:'80px',
+    duration:2000,
+    delay:200,
+});
+ScrollReveal().reveal('.home-content, .heading', {origin: 'top'});
+ScrollReveal().reveal('.home-img , .project-box , .contact form', {origin: 'bottom'});
+ScrollReveal().reveal('.home-contact h1, .about-img',{origin:'left'});
+ScrollReveal().reveal('.home-contact p, .about-content',{origin:'right'});
 
+
+// type js---------------------------------------------------------
+const typed =new Typed('.multiple-text',
+    {
+        strings:['Frontend Developer','Backend Developer','Web Developer','Python Developer'],
+        typeSpeed:70,
+        backSpeed:70,
+        backDelay:1000,
+        loop:true,
+    }
+);
