637643883503e9266f094c7017f63fcd1033897e
Diksha kushwah <dikshakushwah2001@gmail.com>
2024-07-05 15:44:00 +0530

    commit

--- js.js
+++
@@ -1,9 +0,0 @@
-function toggleContent() {
-    var contentDiv = document.getElementById("content");
-    if (contentDiv.innerHTML.trim() === "This is the default content.") {
-      contentDiv.innerHTML = "<p>This is the updated content.</p>";
-    } else {
-      contentDiv.innerHTML = "<p>This is the default content.</p>";
-    }
-  }
-  
